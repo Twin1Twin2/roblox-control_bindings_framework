77f52fb769de29f6a916f5ef5295ab8d5865e90e
Twin1Twin2 <theawesometwin827@gmail.com>
2017-09-07 01:04:35 -0700

    Added more to installer.lua

--- installer.lua
+++ installer.lua
@@ -21,10 +21,33 @@ local HttpService   = game:GetService("HttpService")
 local FILELIST_URL  = ""
 
 
-local httpEnabled = http.HttpEnabled
+local httpEnabled   = http.HttpEna  bled
 HttpService.HttpEnabled = true
 
+local function CreateModuleScript(scriptName, sourceURL)
+    local source    = HTTPService:GetAsync(sourceURL, true)
+    
+    local moduleScript  = Instance.new("ModuleScript")
+        moduleScript.Source = source
+        moduleScript.Name   = scriptName
 
+    return moduleScript
+end
+
+local fileListJSON  = HTTPService:GetAsync(FILELIST_URL, true)
+local fileList      = HttpService:JSONDecode(fileListJSON)
+
+local URL               = fileList.url
+local moduleExtension   = fileList.modules_folder
+local mainPath          = fileList.main
+local modulesPaths      = fileList.modules
+
+
+
+
+for _, module in pairs(modulesPaths) do
+
+end
 
 
 HttpService.HttpEnabled = httpEnabled
